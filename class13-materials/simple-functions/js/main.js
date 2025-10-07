562e3f03fef2cd593dda45454991a406e6b62197
Melissa Pignalosa <m.pignalosa@hotmail.com>
2025-10-07 12:18:23 +1030

    Basic function practice

--- class13-materials/simple-functions/js/main.js
+++ class13-materials/simple-functions/js/main.js
@@ -1,5 +1,20 @@
 //create a function that adds two numbers and alerts the sum
+function addAndAlert(num1, num2){
+    alert(num1 + num2)
+}
+
+addAndAlert(4,5)
 
 //create a function that multiplys three numbers and console logs the product
+function multiplyThree(num1, num2, num3){
+    console.log(num1 * num2 * num3)
+}
 
-//create a function that divides two numbers and returns the ???
+multiplyThree(10, 3, 4);
+
+//create a function that divides two numbers and returns the remainder
+function divideTwo(num1, num2){
+    return num1 % num2;
+}
+
+console.log(divideTwo(8, 3))
